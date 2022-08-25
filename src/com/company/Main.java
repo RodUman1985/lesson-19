b10fc370fa6d966d56ebbc651c3d2b38caa3b2fa
RodUman1985 <108888005+RodUman1985@users.noreply.github.com>
2022-08-25 21:11:41 +0300

    less19.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,15 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 	// write your code here
 MyList list= new MyList();
 list.add(1);
@@ -21,5 +26,23 @@ list.print();
         System.out.println(date);
         Date date2= new Date(452352352);
         System.out.println(date2);
+  // форматирование даты
+        SimpleDateFormat format = new SimpleDateFormat(" yyyy ## MMM ## HH : mm  G ");
+        format.format(date);
+        System.out.println(format.format(date));
+       //  format = new SimpleDateFormat("MM yy dd");
+       // Scanner sc= new Scanner(System.in);
+       // String s=sc.nextLine();
+      // Date d = format.parse(s);
+       // System.out.println(d);
+        //класс Calendar (абстрактный(!))
+        Calendar c =Calendar.getInstance();
+        System.out.println(c);
+        c.set(Calendar.MONTH,Calendar.SEPTEMBER);
+        c.set(Calendar.DAY_OF_MONTH,3);
+        System.out.println(c);
+        c.add(Calendar.YEAR,1);
+        c.add (Calendar.HOUR, -2);
+        System.out.println(c);
     }
 }
